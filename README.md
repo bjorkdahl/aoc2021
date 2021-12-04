b6ead08ea8df8ae305f979c1221610260bccf9b8
Adam Björkdahl <adam.snith@gmail.com>
2021-12-04 16:24:15 +0100

    Day 3

--- README.md
+++ README.md
@@ -11,9 +11,9 @@
 
 <!--SOLUTIONS-->
 
-![Day](https://badgen.net/badge/01/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -69,9 +69,9 @@ yarn start 1
 
 ```
 Day 01
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.35ms
+Time part 2: 0.35ms
+Both parts: 0.7ms
 ```
 
 ```
@@ -83,9 +83,9 @@ Both parts: 3.12ms
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1.25ms
+Time part 2: 1.79ms
+Both parts: 3.04ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 3.12ms
+Total stars: 6/50
+Total time: 6.86ms
 ```
 
 <!--/RESULTS-->
--- src/day03/index.ts
+++ src/day03/index.ts
@@ -1,17 +1,72 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) =>
+  rawInput.split(/\n/).map((a) => a.trim());
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const result = { gammaRate: "", epsilonRate: "" };
+
+  const map = input.reduce((acc, binary) => {
+    for (let i = 0; i < binary.length; i++) {
+      const currentSum = acc.get(i) ?? 0;
+      acc.set(i, currentSum + Number(binary.at(i)));
+    }
+
+    return acc;
+  }, new Map<number, number>());
+
+  for (const [key, value] of map.entries()) {
+    result.gammaRate += value / input.length > 0.5 ? "1" : "0";
+    result.epsilonRate += value / input.length < 0.5 ? "1" : "0";
+  }
+
+  return parseInt(result.epsilonRate, 2) * parseInt(result.gammaRate, 2);
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  let input = parseInput(rawInput);
 
-  return;
+  let OGRating = Array.from(input),
+    CO2Rating = Array.from(input);
+
+  while (OGRating.length > 1 && CO2Rating.length > 1) {
+    const length = 12;
+    let OGsum = 0,
+      CO2sum = 0;
+
+    for (let i = 0; i < length; i++) {
+      for (const binary of OGRating) {
+        OGsum += Number(binary.at(i));
+      }
+
+      for (const binary of CO2Rating) {
+        CO2sum += Number(binary.at(i));
+      }
+
+      if (OGRating.length > 1) {
+        if (OGsum / OGRating.length >= 0.5) {
+          OGRating = OGRating.filter((binary) => binary.at(i) === "1");
+        } else {
+          OGRating = OGRating.filter((binary) => binary.at(i) === "0");
+        }
+      }
+
+      if (CO2Rating.length > 1) {
+        if (CO2sum / CO2Rating.length >= 0.5) {
+          CO2Rating = CO2Rating.filter((binary) => binary.at(i) === "0");
+        } else {
+          CO2Rating = CO2Rating.filter((binary) => binary.at(i) === "1");
+        }
+      }
+
+      OGsum = 0;
+      CO2sum = 0;
+    }
+  }
+
+  return parseInt(OGRating[0], 2) * parseInt(CO2Rating[0], 2);
 };
 
 run({
